6b80ec5e5675af93e13936a61eebb799023bef99
Andre Parron <andreparoon@gmail.com>
2024-06-18 17:41:52 -0300

    first commit

---
+++ FrontEnd/frontend-plataformapagamento/src/App.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./styles/global.scss";
+import Header from "./components/Header/Header";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <div className="App">
+        <h1>Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ FrontEnd/frontend-plataformapagamento/src/components/Header/Header.tsx
@@ -0,0 +1,16 @@
+import './Header.scss';
+
+function Header(){
+    return(
+        <>
+        <div className="container">
+            <div className="content">
+                <h1>Conteudo do header</h1>
+            </div>
+        </div>
+        </>
+    );
+}
+
+export default Header;
+
