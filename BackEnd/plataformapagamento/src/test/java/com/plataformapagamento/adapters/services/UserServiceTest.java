113a4b849392c1d2fe8d4d75242925fd56505a6d
Andre Parron <andreparoon@gmail.com>
2024-06-25 17:53:44 -0300

    Fix: Ajustando teste de acordo com a nova forma de retorno de informações.

--- BackEnd/plataformapagamento/src/test/java/com/plataformapagamento/adapters/services/UserServiceTest.java
+++ BackEnd/plataformapagamento/src/test/java/com/plataformapagamento/adapters/services/UserServiceTest.java
@@ -1,5 +1,8 @@
 package com.plataformapagamento.adapters.services;
 
+import com.plataformapagamento.adapters.DTOs.user.UserDeleteDTO;
+import com.plataformapagamento.adapters.DTOs.user.UserRequestDTO;
+import com.plataformapagamento.adapters.DTOs.user.UserResponseDTO;
 import com.plataformapagamento.adapters.repositories.UserRepository;
 import com.plataformapagamento.domain.user.User;
 import com.plataformapagamento.domain.user.UserType;
@@ -12,10 +15,18 @@ import org.mockito.Captor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.containsInAnyOrder;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 class UserServiceTest {
@@ -56,6 +67,172 @@ class UserServiceTest {
             // Act & Assert
             Exception exception = assertThrows(Exception.class, () -> userService.ValidateTransaction(user, amount));
             assertEquals("Saldo Insuficiente para a transação.", exception.getMessage());
+        }
+
+    }
+    @Nested
+    class FindUserById {
+
+        @Test
+        @DisplayName("Should return user when user is found")
+        void shouldReturnUserWhenUserIsFound() throws Exception {
+            // Arrange
+            User user = new User();
+            when(userRepository.findUserById(any(Long.class))).thenReturn(Optional.of(user));
+
+            // Act
+            User result = userService.findUserById(1L);
+
+            // Assert
+            assertNotNull(result);
+            assertEquals(user, result);
+        }
+
+        @Test
+        @DisplayName("Should throw exception when user is not found")
+        void shouldThrowExceptionWhenUserIsNotFound() {
+            // Arrange
+            when(userRepository.findUserById(any(Long.class))).thenReturn(Optional.empty());
+
+            // Act & Assert
+            Exception exception = assertThrows(Exception.class, () -> userService.findUserById(1L));
+            assertEquals("Usuario nao encontrado.", exception.getMessage());
+        }
+    }
+
+    @Nested
+    class CreateUser {
+
+        @Test
+        @DisplayName("Should create user successfully")
+        void shouldCreateUserSuccessfully() throws Exception {
+            // Arrange
+            UserRequestDTO requestDTO = new UserRequestDTO(
+                    "John",
+                    "Doe",
+                    "123456789",
+                    "john.doe@example.com",
+                    "password",
+                    BigDecimal.valueOf(1000),
+                    UserType.COMUM
+            );
+            User user = new User(requestDTO);
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            String encryptedPassword = passwordEncoder.encode(requestDTO.password());
+            user.setPassword(encryptedPassword);
+
+            when(userRepository.save(userArgumentCaptor.capture())).thenReturn(user);
+
+            // Act
+            UserResponseDTO result = userService.createUser(requestDTO);
+
+            // Assert
+            assertNotNull(result);
+            assertTrue(passwordEncoder.matches(requestDTO.password(), result.password()));
+        }
+
+        @Test
+        @DisplayName("Should throw exception when user already exists")
+        void shouldThrowExceptionWhenUserAlreadyExists() {
+            // Arrange
+            UserRequestDTO requestDTO = new UserRequestDTO(
+                    "John",
+                    "Doe",
+                    "123456789",
+                    "john.doe@example.com",
+                    "password",
+                    BigDecimal.valueOf(1000),
+                    UserType.COMUM
+            );
+            when(userRepository.save(any())).thenThrow(new DataIntegrityViolationException(""));
+
+            // Act & Assert
+            Exception exception = assertThrows(Exception.class, () -> userService.createUser(requestDTO));
+            assertEquals("Usuario já cadastrado.", exception.getMessage());
+        }
+    }
+
+    @Nested
+    class GetAllUser {
+
+        @Test
+        @DisplayName("Should return all users")
+        void shouldReturnAllUsers() {
+            // Arrange
+            User user = new User();
+            List<User> users = List.of(user);
+            when(userRepository.findAll()).thenReturn(users);
+
+            // Act
+            List<UserResponseDTO> result = userService.getAllUser();
+
+            // Assert
+            assertNotNull(result);
+            assertEquals(1, result.size());
+            assertThat(result, containsInAnyOrder(new UserResponseDTO(user)));
+        }
+    }
+
+    @Nested
+    class SaveUser {
+
+        @Test
+        @DisplayName("Should save user successfully")
+        void shouldSaveUserSuccessfully() {
+            // Arrange
+            User user = new User();
+
+            // Act
+            userService.saveUser(user);
+
+            // Assert
+            verify(userRepository, times(1)).save(user);
+        }
+    }
+
+    @Nested
+    class DeleteUser {
+
+        @Test
+        @DisplayName("Should delete user successfully")
+        void shouldDeleteUserSuccessfully() throws Exception {
+            // Arrange
+            User user = new User();
+            UserDeleteDTO deleteDTO = new UserDeleteDTO(1L);
+            when(userRepository.findUserById(any(Long.class))).thenReturn(Optional.of(user));
+            doNothing().when(userRepository).deleteById(any(Long.class));
+
+            // Act
+            User result = userService.deleteUser(deleteDTO);
+
+            // Assert
+            assertNotNull(result);
+            verify(userRepository, times(1)).deleteById(any(Long.class));
+        }
+
+        @Test
+        @DisplayName("Should throw exception when user cannot be deleted")
+        void shouldThrowExceptionWhenUserCannotBeDeleted() {
+            // Arrange
+            UserDeleteDTO deleteDTO = new UserDeleteDTO(1L);
+            when(userRepository.findUserById(any(Long.class))).thenReturn(Optional.of(new User()));
+            doThrow(new DataIntegrityViolationException("")).when(userRepository).deleteById(any(Long.class));
+
+            // Act & Assert
+            Exception exception = assertThrows(Exception.class, () -> userService.deleteUser(deleteDTO));
+            assertEquals("Não é possível deletar o usuario.", exception.getMessage());
+        }
+
+        @Test
+        @DisplayName("Should throw exception when user is not found")
+        void shouldThrowExceptionWhenUserIsNotFound() {
+            // Arrange
+            UserDeleteDTO deleteDTO = new UserDeleteDTO(1L);
+            when(userRepository.findUserById(any(Long.class))).thenReturn(Optional.empty());
+
+            // Act & Assert
+            Exception exception = assertThrows(Exception.class, () -> userService.deleteUser(deleteDTO));
+            assertEquals("Usuario nao encontrado.", exception.getMessage());
         }
     }
 }
