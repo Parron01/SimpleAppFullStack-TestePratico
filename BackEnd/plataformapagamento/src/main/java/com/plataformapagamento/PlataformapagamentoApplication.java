e70ede457e47475895261f9241e68dcf4d9f7cd4
Andre Parron <andreparoon@gmail.com>
2025-04-15 19:12:11 -0400

    Update PlataformapagamentoApplication.java

--- BackEnd/plataformapagamento/src/main/java/com/plataformapagamento/PlataformapagamentoApplication.java
+++ BackEnd/plataformapagamento/src/main/java/com/plataformapagamento/PlataformapagamentoApplication.java
@@ -9,22 +9,21 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class PlataformapagamentoApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PlataformapagamentoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PlataformapagamentoApplication.class, args);
+    }
 
-
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**")
-						.allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
-						.allowedOrigins("http://localhost:3000", "https://simpleapp.parron01.com")
-						.allowedHeaders("*")
-						.allowCredentials(true);
-			}
-		};
-	}
+    @Bean
+    public static WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                        .allowedOrigins("http://localhost:3000", "https://simpleapp.parron01.com")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
 }
